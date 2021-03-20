11e7c2c6d7addd8b58176064e35b31bedfb5eaaa
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-03-20 20:31:57 +0530

    New Staff App Procedure *Heavy Beta

--- cogs/StaffApp.py
+++ cogs/StaffApp.py
@@ -9,55 +9,17 @@ class StaffApp(commands.Cog):
     # Staff App Command
     @commands.command()
     async def staffapp(self, ctx):
-        try:
-            await ctx.send(
-                "Send `Yes` to confirm/start the staff application! You have 5 seconds to reply with yes."
-            )
-
-            def check(message):
-                return ctx.author == message.author and message.content.lower(
-                ) == "yes"
-
-            msg = await self.bot.wait_for('message', check=check, timeout=5.0)
-        except asyncio.TimeoutError:
-            await ctx.send("Next time rethink before sending that command!")
-            return
-        else:
-            pass
-
-        try:
-            dm = await ctx.author.create_dm()
-            app = "Consider this as the staff app. I am too lazy to fill it rn kekw. Also you have 20 mins to fill this app and if you get late. don't worry just run the command again and have the answers copied."
-            await dm.send(app)
-            await ctx.send("Please check your dms to continue the application!")
-        except:
-            await ctx.send("Open your dms bruh!")
-            return
-
-        try:
-
-            def check2(m):
-                return m.author == ctx.author and m.guild == None
-
-            the_app = await self.bot.wait_for('message', check=check2, timeout=1200.0)
-        except asyncio.TimeoutError:
-            await ctx.send("Action Timeouted!")
-            await dm.send("Action Timeouted!")
-            return
-        else:
-            pass
-
-        await dm.send("We have received your application!")
-        logchannel = await self.bot.fetch_channel(820756992897450005)
-        emb = discord.Embed(title='Staff App Received!',
-                            description=f'''
-    **App from {ctx.author.mention}!**
-
-    {the_app.content}
-      ''',
-                            color=0x00ff00)
-        await logchannel.send(embed=emb)
-        return
+        emb1 = discord.Embed(title="Staff Application!", description="React to the below 📜 paper to start the process! If you can't start the process, there may be some other person applying so please wait!", color= 0x00ff00)
+        emb1.set_footer(text='OwO', icon_url=f'{self.bot.user.avatar_url}')
+        emb = await ctx.send(embed=emb1)
+        await emb.add_reaction('📜')
+        await ctx.message.delete()
+        def check(reaction, user):
+            return str(reaction.emoji) == '📜'
+        while True:
+            rr1, applier = await self.bot.wait_for('reaction_add', check=check)
+            applier_dm = await applier.create_dm()
+            await applier_dm.send("This is a test message")
 
 def setup(bot):
     bot.add_cog(StaffApp(bot))
