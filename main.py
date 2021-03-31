da8e2349699c40b77bdf11be090af2552f0112f7
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-03-31 07:42:28 +0530

    Added logs for error messages

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 # The discord module
 import discord
 # Gets the commands and tasks module from the discord module
