ecfde396710b0f26d9b74a15e9cf239c807bcf70
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-04-09 15:08:28 +0530

    Switched from json files to sqlite db for reaction roles

--- cogs/ReactionRole.py
+++ cogs/ReactionRole.py
@@ -1,8 +1,21 @@
-import json
-
 import discord
 from discord.ext import commands
+import sqlite3
 
+db = sqlite3.connect('main.db')
+c = db.cursor()
+
+c.execute("""
+        CREATE TABLE IF NOT EXISTS reactrole (
+            role_name TEXT,
+            role_id INTEGER,
+            emoji TEXT,
+            message_id INTEGER,
+            guild_id INTEGER
+        )""")
+
+db.commit()
+db.close()
 
 class ReactionRole(commands.Cog):
     def __init__(self, bot):
@@ -11,22 +24,15 @@ class ReactionRole(commands.Cog):
     @commands.command(aliases=['rr', 'reactrole'])
     @commands.has_permissions(manage_roles=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def _reactrole(self, ctx, emoji, role: discord.Role, messageid):
+    async def _reactrole(self, ctx, emoji: discord.Emoji, role: discord.Role, messageid):
         msg = await ctx.channel.fetch_message(messageid)
         await msg.add_reaction(emoji)
-        with open('cogs/reactrole.json') as json_file:
-            data = json.load(json_file)
-            new_react_role = {
-                'role_name': role.name,
-                'role_id': role.id,
-                'emoji': emoji,
-                'message_id': messageid
-            }
+        db = sqlite3.connect('main.db')
+        c = db.cursor()
 
-            data.append(new_react_role)
-
-        with open('cogs/reactrole.json', 'w') as f:
-            json.dump(data, f)
+        c.execute("INSERT INTO reactrole VALUES (?,?,?,?,?)", (role.name, role.id, str(emoji), messageid, ctx.guild.id))
+        db.commit()
+        db.close()
 
         await ctx.message.delete()
 
@@ -49,20 +55,37 @@ class ReactionRole(commands.Cog):
     async def on_raw_reaction_add(self, payload):
         if payload.member.bot:
             return
-        with open('cogs/reactrole.json') as react_file:
-            data = json.load(react_file)
+
+        db = sqlite3.connect('main.db')
+        c = db.cursor()
+
+        c.execute("SELECT * FROM reactrole WHERE guild_id = ?", (payload.guild_id,))
+        data = c.fetchall()
+        db.close()
+
+        if not data:
+            return
+        else:
             for x in data:
-                if x['emoji'] == payload.emoji.name and int(x['message_id']) == payload.message_id:
-                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x['role_id'])
+                if x[2] == str(payload.emoji) and int(x[3]) == payload.message_id:
+                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x[1])
                     await payload.member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-        with open('cogs/reactrole.json') as react_file:
-            data = json.load(react_file)
+        db = sqlite3.connect('main.db')
+        c = db.cursor()
+
+        c.execute("SELECT * FROM reactrole WHERE guild_id = ?", (payload.guild_id,))
+        data = c.fetchall()
+        db.close()
+
+        if not data:
+            return
+        else:
             for x in data:
-                if x['emoji'] == payload.emoji.name and int(x['message_id']) == payload.message_id:
-                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x['role_id'])
+                if str(payload.emoji)[-1:] in x[2] and int(x[3]) == payload.message_id:
+                    role = discord.utils.get(self.bot.get_guild(payload.guild_id).roles, id=x[1])
                     await self.bot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
 
 
