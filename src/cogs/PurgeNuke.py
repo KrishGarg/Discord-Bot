88918e4c7797dc52a44bf64962843ad9242d125a
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-08-04 19:33:21 +0530

    MORE INFOOOO

--- src/cogs/PurgeNuke.py
+++ src/cogs/PurgeNuke.py
@@ -8,10 +8,17 @@ class PurgeNuke(commands.Cog):
         self.bot = bot
 
     # Purge Command
-    @commands.command()
+    @commands.command(
+        name="Purge Messages Command",
+        description="A command to delete some messages in bulk quickly.",
+        usage="purge <number_of_messages_to_delete>",
+        aliases=[
+            "purge"
+        ]
+    )
     @commands.has_permissions(manage_messages=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def purge(self, ctx, num: int, user: discord.Member = None):
+    async def _purge(self, ctx, num: int, user: discord.Member = None):
 
         if user:
             check_func = lambda m: m.author == user and not m.pinned
@@ -49,8 +56,8 @@ class PurgeNuke(commands.Cog):
             return
 
     # Purge Command error handling
-    @purge.error
-    async def purge_error(self, ctx, error):
+    @_purge.error
+    async def _purge_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You can't run that!")
             return
@@ -63,41 +70,44 @@ class PurgeNuke(commands.Cog):
             await ctx.send("Input was wrong! Check the command again!")
 
     # Nuke command
-    @commands.command()
+    @commands.command(
+        name="Nuke command",
+        description="A command to delete and then remake a channel.",
+        usage="nuke",
+        aliases=[
+            "nuke"
+        ]
+    )
     @commands.has_permissions(manage_channels=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def nuke(self, ctx, errorHandling=None):
-        if errorHandling is None:
-            await ctx.reply(
-                "Send me a conformation by sending `Yes`. You have 10 seconds!")
+    async def _nuke(self, ctx):
+        await ctx.reply(
+            "Send me a conformation by sending `Yes`. You have 10 seconds!")
 
-            try:
-                x = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author, timeout=10.0)
-            except asyncio.TimeoutError:
-                await ctx.send(
-                    "I cancelled it because I didn't get any confirmation!")
-                return
+        try:
+            x = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author, timeout=10.0)
+        except asyncio.TimeoutError:
+            await ctx.send(
+                "I cancelled it because I didn't get any confirmation!")
+            return
 
-            if x.content.lower() == "yes":
-                old_channel = ctx.channel
-                new_channel = await old_channel.clone(reason="Has been NUKED!")
-                await old_channel.delete()
-                embed1 = discord.Embed(title="NUKED!",description=f'**This channel was nuked by {ctx.author.mention}.**',color=0x00ff00)
-                embed1.set_footer(text=f'{self.bot.user.name}',icon_url=self.bot.user.avatar_url)
-                embed1.set_image(url="https://media2.giphy.com/media/HhTXt43pk1I1W/giphy.gif?cid=ecf05e47l2gij7b6xv29vavlho3z6mxdi8bdm3o626p2pfcb&rid=giphy.gif&ct=g")
-                await new_channel.send(embed=embed1)
-                return
-
-            else:
-                await ctx.send("Noob! Always think before running these types of commands!!")
-                return
+        if x.content.lower() == "yes":
+            old_channel = ctx.channel
+            new_channel = await old_channel.clone(reason="Has been NUKED!")
+            await old_channel.delete()
+            embed1 = discord.Embed(title="NUKED!",description=f'**This channel was nuked by {ctx.author.mention}.**',color=0x00ff00)
+            embed1.set_footer(text=f'{self.bot.user.name}',icon_url=self.bot.user.avatar_url)
+            embed1.set_image(url="https://media2.giphy.com/media/HhTXt43pk1I1W/giphy.gif?cid=ecf05e47l2gij7b6xv29vavlho3z6mxdi8bdm3o626p2pfcb&rid=giphy.gif&ct=g")
+            await new_channel.send(embed=embed1)
+            return
 
         else:
-            raise commands.CommandError()
+            await ctx.send("Noob! Always think before running these types of commands!!")
+            return
 
     # Nuke command error handling
-    @nuke.error
-    async def nuke_error(self, ctx, error):
+    @_nuke.error
+    async def _nuke_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("Ha! You can't do it!")
             return
