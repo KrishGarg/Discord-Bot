5b78476ac6ffeb4dff8b2264f7d51a1a5718fe25
Krish Garg <65218584+KrishGarg@users.noreply.github.com>
2021-08-06 15:42:08 +0530

    I was wondering where my logs were going....

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ load_dotenv()
 # Logging
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='../discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
